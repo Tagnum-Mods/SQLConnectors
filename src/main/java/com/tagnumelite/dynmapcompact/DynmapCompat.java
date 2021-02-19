7220f3f2c8bf15b36961439f1fba9fcbe037587e
Tagan Hoyle <tagnum.elite@gmail.com>
2021-02-19 20:32:27 +0200

    Initial Commit
    Fixes MySQL connector not existing in forge

---
+++ src/main/java/com/tagnumelite/dynmapcompact/DynmapCompat.java
@@ -0,0 +1,10 @@
+package com.tagnumelite.dynmapcompact;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("dynmapcompact")
+public class DynmapCompat
+{
+    public DynmapCompat() {}
+}
